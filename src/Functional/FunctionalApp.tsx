575469c9ef466ef3f68733a39ab9ae69b3830823
Ethan Groene <ethangroene@gmail.com>
2023-11-06 23:15:55 -0600

    Define allDogs state here, pass it, setter
    Also, display FunctionalDogs / FunctionalCreateDogForm components based on value of dogsAreDisplayed, which user will eventually be able to change by clicking on navbar options.

--- src/Functional/FunctionalApp.tsx
+++ src/Functional/FunctionalApp.tsx
@@ -1,18 +1,37 @@
+import { useState, useEffect } from "react";
 import { FunctionalCreateDogForm } from "./FunctionalCreateDogForm";
 import { FunctionalDogs } from "./FunctionalDogs";
 import { FunctionalSection } from "./FunctionalSection";
+import { Dog } from "../types";
+import { getAllDogs } from "../Shared/Requests";
 
 export function FunctionalApp() {
+  const [allDogs, setAllDogs] = useState<Dog[]>([]);
+
+  useEffect(() => {
+    getAllDogs().then(setAllDogs);
+  }, []);
+
+  // State to determine if (favs/unfavs) or 'create dog' displays:
+  // Pass setter to FunctionalSection. There, call on click of 'favs', 'unfavs', & 'create dog' btns
+  const [dogsAreDisplayed, setDogsAreDisplayed] = useState<boolean>(true);
+
   return (
     <div className="App" style={{ backgroundColor: "skyblue" }}>
       <header>
         <h1>pup-e-picker (Functional)</h1>
       </header>
       <section id="main-section">
-        <FunctionalSection />
+        <FunctionalSection
+          allDogs={allDogs}
+          setDogsAreDisplayed={setDogsAreDisplayed}
+        />
         <div className="content-container">
-          <FunctionalDogs />
-          <FunctionalCreateDogForm />
+          {dogsAreDisplayed ? (
+            <FunctionalDogs allDogs={allDogs} setAllDogs={setAllDogs} />
+          ) : (
+            <FunctionalCreateDogForm />
+          )}
         </div>
       </section>
     </div>
