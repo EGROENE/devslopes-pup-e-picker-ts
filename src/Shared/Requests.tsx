319ae3cee876bbeebcbd9ac2f7d4bd864381a671
Ethan Groene <ethangroene@gmail.com>
2023-11-07 23:56:26 -0600

    Create method to delete dog from db

--- src/Shared/Requests.tsx
+++ src/Shared/Requests.tsx
@@ -1,6 +1,17 @@
 // Method to fetch all dogs from dogs array in db.json:
 export const getAllDogs = () => {
-  return fetch("http://localhost:3000/dogs").then((response) =>
-    response.json()
-  );
+  return fetch("http://localhost:3000/dogs").then((response) => response.json());
+};
+
+export const deleteDog = (dogID: number) => {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-type", "application/json");
+
+  var requestOptions = {
+    method: "DELETE",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return fetch(`http://localhost:3000/dogs/${dogID}`, requestOptions);
 };
