ac7c32c78b9f0db0d205d8fb89e6cdfff96a79c8
Ethan Groene <ethangroene@gmail.com>
2023-11-08 00:16:23 -0600

    Type existing request methods

--- src/Shared/Requests.tsx
+++ src/Shared/Requests.tsx
@@ -1,9 +1,9 @@
 // Method to fetch all dogs from dogs array in db.json:
-export const getAllDogs = () => {
+export const getAllDogs = (): Promise<any> => {
   return fetch("http://localhost:3000/dogs").then((response) => response.json());
 };
 
-export const deleteDog = (dogID: number) => {
+export const deleteDog = (dogID: number): Promise<Response> => {
   var myHeaders = new Headers();
   myHeaders.append("Content-type", "application/json");
 
